2402bf8ed6b675da8ae2da2659ee481b1ecd982b
aguiraf <aguiraf@gmail.com>
2021-05-13 11:23:48 +0200

    Add id, created_at and updated_at columns

--- create.sql
+++ create.sql
@@ -2,7 +2,7 @@
 DROP TABLE public.nodi;
 CREATE TABLE public.nodi
 (
-    "idNodo" bigint NOT NULL,
+    "idNodo" bigint PRIMARY KEY,
     "codNodo" character varying COLLATE pg_catalog."default" NOT NULL,
     "denominazione" character varying COLLATE pg_catalog."default",
     "tipoNodo" character varying COLLATE pg_catalog."default",
@@ -10,6 +10,8 @@ CREATE TABLE public.nodi
     "denominazioneComune" character varying COLLATE pg_catalog."default",
     "codIstatProvincia" character varying COLLATE pg_catalog."default",
     "siglaProvincia" character varying COLLATE pg_catalog."default",
+    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMP NOT NULL DEFAULT NOW(),
     lat numeric(7,5),
     lng numeric(7,5),
     geom geometry(POINT, 4326),
@@ -23,10 +25,12 @@ CREATE TABLE public.nodi
 DROP TABLE public.fermate;
 CREATE TABLE public.fermate
 (   
-    "idFermata" bigint NOT NULL,
+    "idFermata" bigint PRIMARY KEY,
     "codFermata" bigint NOT NULL,
     "desc" character varying COLLATE pg_catalog."default",
     "codNodo" character varying COLLATE pg_catalog."default",
+    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMP NOT NULL DEFAULT NOW(),
     lat numeric(7,5),
     lng numeric(7,5),
     geom geometry(POINT, 4326),
@@ -39,14 +43,44 @@ CREATE TABLE public.fermate
 DROP TABLE public.paline;
 CREATE TABLE public.paline
 (
-    "idPalina" bigint NOT NULL,
+    "idPalina" bigint PRIMARY KEY,
     "desc" character varying COLLATE pg_catalog."default",
     azienda character varying COLLATE pg_catalog."default",
     "codCsrAzienda" smallint,
     "codFermata" bigint,
+    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMP NOT NULL DEFAULT NOW(),
     lat numeric(7,5),
     lng numeric(7,5),
     geom geometry(POINT, 4326),
     -- geog geography(point,4326),
     CONSTRAINT paline_pkey PRIMARY KEY ("idPalina")
 );
+
+
+-- https://x-team.com/blog/automatic-timestamps-with-postgresql/
+CREATE OR REPLACE FUNCTION trigger_set_timestamp()
+RETURNS TRIGGER AS $$
+BEGIN
+  NEW.updated_at = NOW();
+  RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+
+CREATE TRIGGER set_timestamp
+BEFORE UPDATE ON nodi
+FOR EACH ROW
+EXECUTE PROCEDURE trigger_set_timestamp();
+
+
+CREATE TRIGGER set_timestamp
+BEFORE UPDATE ON fermate
+FOR EACH ROW
+EXECUTE PROCEDURE trigger_set_timestamp();
+
+
+CREATE TRIGGER set_timestamp
+BEFORE UPDATE ON paline
+FOR EACH ROW
+EXECUTE PROCEDURE trigger_set_timestamp();
